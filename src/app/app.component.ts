eb95eeff768cadac96f67446fce8e1828dc24906
Ismail Metaich <ismail@codinglab.com>
2023-06-13 20:53:05 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,59 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'coding-challenge-v2';
+  tasks = [
+    {
+      title: 'task 1',
+      completed: false,
+    },
+    {
+      title: 'task 2',
+      completed: false,
+    },
+    {
+      title: 'task 3',
+      completed: false,
+    },
+    {
+      title: 'task 4',
+      completed: false,
+    },
+  ];
+
+  completedTasks = [
+    {
+      title: 'task 5',
+      completed: false,
+    },
+    {
+      title: 'task 6',
+      completed: false,
+    },
+    {
+      title: 'task 7',
+      completed: false,
+    },
+    {
+      title: 'task 8',
+      completed: false,
+    },
+  ];
+
+  addTask(description: string) {
+    this.tasks.push({
+      title: description,
+      completed: false,
+    });
+  }
+
+  deleteTask(task: any) {
+    let index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
